a9f4088638bf55fa0cf201682dbe50b47d0c5d31
Addy Darmawan <addy@makankor.net>
2016-01-20 14:17:41 +0700

    Display real country names and cities instead of codes

--- app/src/main/java/net/makankor/localecheck/MainActivity.kt
+++ app/src/main/java/net/makankor/localecheck/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import java.util.*
@@ -13,6 +14,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     var recyclerView: RecyclerView? = null
+    var textDefault: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         recyclerView = findViewById(R.id.recycler_view) as RecyclerView
+        textDefault = findViewById(R.id.text_default) as TextView
         setupView()
     }
 
@@ -29,15 +32,20 @@ class MainActivity : AppCompatActivity() {
         recyclerView!!.adapter = CustomAdapter()
     }
 
-    class CustomAdapter : RecyclerView.Adapter<VH>() {
+    inner class CustomAdapter : RecyclerView.Adapter<VH>() {
         val list = ArrayList<Locale>()
 
-       init {
-           list.add(Locale.getDefault())
-           val local = Locale.getAvailableLocales();
-           for (curLocale in local) {
-                list.add(curLocale)
-           }
+        init {
+            textDefault!!.text = Locale.getDefault().language
+
+            val local = Locale.getAvailableLocales();
+            for (curLocale in local) {
+                if (!curLocale.displayCountry.trim().isEmpty()) {
+                    list.add(curLocale)
+                }
+            }
+
+            Collections.sort(list, Comparator { t, t2 -> t.displayCountry.compareTo(t2.displayCountry) })
         }
 
         override fun getItemCount(): Int {
@@ -45,21 +53,28 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): VH? {
-            val textView = LayoutInflater.from(parent!!.context).inflate(R.layout.text_view, parent, false) as TextView
+            val view = LayoutInflater.from(parent!!.context).inflate(R.layout.table_content, parent, false) as View
 
-            return VH(view = textView)
+            return VH(view)
         }
 
         override fun onBindViewHolder(holder: VH?, position: Int) {
             var curLocale = list.get(position)
 
-            holder!!.view.text = curLocale.displayCountry + "." + curLocale.language
+            holder!!.lblCountry!!.text = curLocale.displayCountry
+            holder!!.lblLanguage!!.text = curLocale.displayLanguage
         }
 
     }
 
-    class VH(view: TextView) : RecyclerView.ViewHolder(view) {
-        val view = view
+    class VH(view: View) : RecyclerView.ViewHolder(view) {
+        var lblCountry: TextView? = null
+        var lblLanguage: TextView? = null
+
+        init {
+            lblCountry = view.findViewById(R.id.lbl_country) as TextView
+            lblLanguage = view.findViewById(R.id.lbl_language) as TextView
+        }
     }
 
 }
